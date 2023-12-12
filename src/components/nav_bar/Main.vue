7320e38a7bbb5a174d65c3828a8f4133a74f8f7b
UZB03 <jasurholiqov03@gmail.com>
2023-12-12 10:46:48 +0500

    img nav

--- src/components/nav_bar/Main.vue
+++ src/components/nav_bar/Main.vue
@@ -4,7 +4,7 @@
     <nav class="bg-white border-gray-200 dark:bg-gray-900 shadow-lg">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
         <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="./icon.jpg" class="w-16" alt="Flowbite Logo" />
+            <img src="./icon.jpg" class="w-16" alt="Flowbite Logo"  style="border-radius: 50%;"/>
             <span class="self-center text-3xl font-semibold whitespace-nowrap" style="color: rgb(215, 215, 2); text-shadow:0 0 5px yellow">Eco Clean</span>
         </a>
         <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
